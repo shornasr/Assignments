02cc7ccbe6e36800e51f775798655c7bea3e0433
shornasr <151051759+shornasr@users.noreply.github.com>
2023-11-16 22:38:29 +0600

    Add files via upload

---
+++ assignment 1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int x = 5;
+
+    printf("value of x = %d/n,x");
+    printf("Adress of x = %d/n,&x");
+
+
+      getch();
+}
---
+++ assignment 2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+
+ {
+
+    int x = 5;
+
+    int *ptr;
+
+    ptr = &x;
+
+    printf("%d/n",x);
+    printf("%d/n",&x);
+    printf("%d/n",ptr);
+    printf("%d/n",*ptr);
+    printf("%d/n",&ptr);
+
+
+       gtch();
+ }
